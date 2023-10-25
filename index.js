1907e640a8b79e364902be0d4a52bd669bf56adf
Anish Pokhrel <anishpokhrel2002@gmail.com>
2023-10-25 00:25:33 -0600

    created js driver file that will run the gameplay

---
+++ index.js
@@ -0,0 +1,69 @@
+/*
+    * Course: SENG 513
+    * Date: Oct 23, 2023
+    * Assignment 2
+    * Name: Anish Pokhrel
+    * UCID: 30115576
+*/
+
+// This the JavaScript "driver" function that runs the game  
+
+// import { player, enemy } from '/Player.js'
+// import { background, shop } from '/Sprite.js';
+// import { loadKeyDownEvents, loadkeyUpEvents } from '/Keys.js'
+
+/*  Track the following attributes:
+        * Game time
+        * Flag to check if game has ended
+        * Player 1 and Player 2 event listeners to check their states
+ */
+
+
+
+/******************************************************************************************** */
+
+// Function to start the game 
+// function beginGame () {
+    // activate animations
+    // update player states
+// }
+
+
+// Now invoke the method
+// beginGame();
+
+
+
+/******************************************************************************************** */
+
+// Function to decrease the timer. If timer reaches 0 announce the winner based on remaining health. If both players have same health, declare it a draw.
+// function decrementTimer() {
+// }
+
+
+/******************************************************************************************** */
+
+// Function to add animations for players, frame by frame 
+// function animate() {
+//     update(player1);   // update player state
+//     update(player2);   // update player2 state
+
+
+//     Check if either player is attacking the other:
+//                  player1.attack(player2)
+//                  player2.attack(player1)
+
+//     if game has not ended, but one or both of the players have died then find the winner
+
+
+/******************************************************************************************** */
+
+
+// Function to find the winner when game has ended or a player has been defeated
+// Inputs: player1, player2, timer
+
+// function findWinnder({ player1, player2, timer }) {
+//     Stop the timer as game ended.
+//     Set gameEnded flag to true.
+//     Display the result (whether player1 or player 2 won, or if it was a draw)
+// }
