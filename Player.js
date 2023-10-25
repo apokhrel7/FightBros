1fa1def2f7faf4f0c3e1a7f7e7f31ce71fe368ea
Anish Pokhrel <anishpokhrel2002@gmail.com>
2023-10-25 00:25:58 -0600

    file to create players and handle player specific logic

---
+++ Player.js
@@ -0,0 +1,66 @@
+/*
+    * Course: SENG 513
+    * Date: Oct 23, 2023
+    * Assignment 2
+    * Name: Anish Pokhrel
+    * UCID: 30115576
+*/
+
+
+// This file creates the functionality for the two players
+
+// initialize gravity for the players (required for when they jump and fall)
+
+// Create class Player that implements class 'Sprite' from the Sprite.js file.
+// This class will store all attributes of the Player, such as health, velocity, their attack state (attacking or not), etc.
+// class Fighter extends Sprite {
+   
+    // Function 'attack()' to handle what happens when a player attacks
+    // Input: opposing player
+    // attack(opposingPlayer) {}
+
+
+    // Function 'isFighting' hanldles logic to check if a player is fighting their opposition.
+    // Input: opposing player
+    // Return: Boolean
+
+    // isFighting(enemyFighter) {}
+
+
+
+    // Function 'movement()' finds if a player is moving to the left, right, or up (jumping).
+    // Input: none
+    // Return: boolean
+
+    // movement() {
+           
+           // if user is pressing right key, go right
+           // if user is pressing left key, go left
+           // if user is pressing space bar, jump up 
+    //     let running = false;    // Determinate if player is running or not.
+    //     if (this.health > 0) {
+    //         if (Object.values(this.keys)[0].pressed && (this.lastKey === 'a' || this.lastKey === 'ArrowLeft') && this.position.x >= 0) {
+    //             this.velocity.x = -this.moveFactor; // 'a' is pressed and it's the last pressed key, then move to the left.
+    //             this.switchSprite('run')
+    //             running = true;
+    //         } else if (Object.values(this.keys)[1].pressed && (this.lastKey === 'd' || this.lastKey === 'ArrowRight') && this.position.x <= (canvas.width - this.width)) {
+    //             this.velocity.x = this.moveFactor;  // 'd' is pressed and it's the last pressed key, then move to the right.
+    //             this.switchSprite('run')
+    //             running = true;
+    //         }
+    //     }
+    //     return running;
+    // }
+
+//}
+
+/******************************************************************************************** */
+
+
+
+// Create player1 by using class 'Player'
+// export const player = new Player({})
+
+// Create player1 by using class 'Player'
+// export const enemy = new Player({})
+
