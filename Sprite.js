e416318c6dfdb644e0e2de89c6d94d14f4ad6038
Anish Pokhrel <anishpokhrel2002@gmail.com>
2023-10-25 00:26:33 -0600

    This file responsible for creating the general sprites used in animation

---
+++ Sprite.js
@@ -0,0 +1,40 @@
+/*
+    * Course: SENG 513
+    * Date: Oct 23, 2023
+    * Assignment 2
+    * Name: Anish Pokhrel
+    * UCID: 30115576
+*/
+
+
+// This file is responsible for creating the general sprites used in animation  
+
+
+// Class 'Sprite' used to initialize sprite attributes (dimensions, scale, position)
+// This class is used to draw sprites in the canvas and update each sprite
+
+// class Sprite {
+
+
+        // Do the following inside the class 'Sprite':
+//     Draw the sprites in the canvas (tag in html file).
+//     draw() {
+//     }
+
+
+//     // For every frame, update sprites
+//     update() {
+//         this.draw();   // call draw method again
+//     }
+// }
+
+
+/******************************************************************************************** */
+
+// Create instance of class Sprite for the background
+
+// export const background = new Sprite({
+// })
+
+
+/******************************************************************************************** */
