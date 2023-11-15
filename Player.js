bd20953a91e7c8b4815e12dc284a0a937457a692
Anish Pokhrel <anishpokhrel2002@gmail.com>
2023-11-15 15:38:01 -0700

    Added constants to player file

--- Player.js
+++ Player.js
@@ -1,69 +1,68 @@
-/*
-    * Course: SENG 513
-    * Date: Oct 23, 2023
-    * Assignment 2
-    * Name: Anish Pokhrel
-    * UCID: 30115576
-*/
+// /*
+//     * Course: SENG 513
+//     * Date: Oct 23, 2023
+//     * Assignment 2
+//     * Name: Anish Pokhrel
+//     * UCID: 30115576
+// */
 
-// This file creates the functionality for the two players
+// // This file creates the functionality for the two players
 
-import Sprite from '/Sprite.js';
-
-
-// Set dimensions of canvas (where the game will be) 
-canvas.width = 1024;
-canvas.height = 576;
+// import Sprite from '/Sprite.js';
 
 
-// initialize gravity for the players (required for when they jump and fall)
-gravity = 0.9;
+// // Set dimensions of canvas (where the game will be) 
+// canvas.width = 1024;
+// canvas.height = 576;
 
-// Create class Player that implements class 'Sprite' from the Sprite.js file.
-// This class will store all attributes of the Player, such as health, velocity, their attack state (attacking or not), etc.
-// class Fighter extends Sprite {
+
+
+
+// // Create class Player that implements class 'Sprite' from the Sprite.js file.
+// // This class will store all attributes of the Player, such as health, velocity, their attack state (attacking or not), etc.
+// // class Fighter extends Sprite {
    
-    // Function 'attack()' to handle what happens when a player attacks
-    // Input: opposing player
-    // attack(opposingPlayer) {}
+//     // Function 'attack()' to handle what happens when a player attacks
+//     // Input: opposing player
+//     // attack(opposingPlayer) {}
 
 
-    // Function 'isFighting' hanldles logic to check if a player is fighting their opposition.
-    // Input: opposing player
-    // Return: Boolean
+//     // Function 'isFighting' hanldles logic to check if a player is fighting their opposition.
+//     // Input: opposing player
+//     // Return: Boolean
 
-    // isFighting(enemyFighter) {}
-
-
-
-    // Function 'movement()' finds if a player is moving to the left, right, or up (jumping).
-    // Input: none
-    // Return: boolean
-
-    // movement() {
-           // if user is pressing right key, go right
-           // if user is pressing left key, go left
-           // if user is pressing space bar, jump up 
-    // }
-
-//}
-
-/******************************************************************************************** */
-
-// Creating player1
-const player1 = new Player({
-    x: 0,
-    y: 0
-})
-
-player1.draw();
+//     // isFighting(enemyFighter) {}
 
 
-const player2 = new Player({
-    x: 0,
-    y: 0
-})
 
-player2.draw();
+//     // Function 'movement()' finds if a player is moving to the left, right, or up (jumping).
+//     // Input: none
+//     // Return: boolean
 
-export default Player;
+//     // movement() {
+//            // if user is pressing right key, go right
+//            // if user is pressing left key, go left
+//            // if user is pressing space bar, jump up 
+//     // }
+
+// //}
+
+// /******************************************************************************************** */
+
+// // Creating player1
+// const player1 = new Player({
+//     x: 0,
+//     y: 0
+// })
+
+// player1.draw();
+
+
+// const player2 = new Player({
+//     x: 0,
+//     y: 0
+// })
+
+// player2.draw();
+
+// export default Player;
