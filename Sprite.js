a191726579f1fa9327faf0e03a02cad56ed693e0
Anish Pokhrel <anishpokhrel2002@gmail.com>
2023-11-14 18:20:33 -0700

    Added player movements

--- Sprite.js
+++ Sprite.js
@@ -1,35 +1,36 @@
-/*
-    * Course: SENG 513
-    * Date: Oct 23, 2023
-    * Assignment 2
-    * Name: Anish Pokhrel
-    * UCID: 30115576
-*/
+// /*
+//     * Course: SENG 513
+//     * Date: Oct 23, 2023
+//     * Assignment 2
+//     * Name: Anish Pokhrel
+//     * UCID: 30115576
+// */
 
 
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+// const canvas = document.querySelector('canvas');
+// const c = canvas.getContext('2d');
 
-// initialize gravity for the players (required for when they jump and fall)
-gravity = 0.9;
-
-
-// This file is responsible for creating the general sprites used in animation  
+// // initialize gravity for the players (required for when they jump and fall)
+// gravity = 0.9;
 
 
-// Class 'Sprite' used to initialize sprite attributes (dimensions, scale, position)
-// This class is used to draw sprites in the canvas and update each sprite
+// // This file is responsible for creating the general sprites used in animation  
+
+
+// // Class 'Sprite' used to initialize sprite attributes (dimensions, scale, position)
+// // This class is used to draw sprites in the canvas and update each sprite
 
 class Sprite {
     constructor({position, velocity}) {
         this.position = position;
         this.velocity = velocity;
         this.height = 150;
+        this.lastKeyPressed;
     }
 
     // Draw the sprites in the canvas (tag in html file).
     draw() {
-        c.fillStyle = "green";  // sprite is green
+        c.fillStyle = 'green';  // sprite is green
         c.fillRect(this.position.x, this.position.y, 50, this.height); 
     }
 
@@ -37,7 +38,10 @@ class Sprite {
     // For every frame, update sprites
     update() {
         this.draw();   // call draw method again
-        this.position.y += this.velocity.y;      // add y velocity
+
+        // Determines how to move players
+        this.position.x += this.velocity.x;   // move in x-direction
+        this.position.y += this.velocity.y;   // move in y-direction
 
         // Prevents sprite from falling down canvas
         // If overall position of sprite is greater than or equal to canvas...
@@ -52,7 +56,7 @@ class Sprite {
     }
 }
 
-const player1 = new Player({
+const player1 = new Sprite({
     position: {
         x: 0,
         y: 0
@@ -64,7 +68,7 @@ const player1 = new Player({
 })
 
 
-const player2 = new Player({
+const player2 = new Sprite({
     position: {
         x: 400,
         y: 100
@@ -77,15 +81,15 @@ const player2 = new Player({
 
 
 
-/******************************************************************************************** */
+// /******************************************************************************************** */
 
-// Create instance of class Sprite for the background
+// // Create instance of class Sprite for the background
 
-// export const background = new Sprite({
-// })
-
-
-/******************************************************************************************** */
+// // export const background = new Sprite({
+// // })
 
 
-export default Sprite;
+// /******************************************************************************************** */
+
+
+// export default Sprite;
